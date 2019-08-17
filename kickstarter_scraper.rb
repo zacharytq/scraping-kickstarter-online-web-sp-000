a120081e548d1a315021d2686bb08cb0b1891a18
chic-page-6502 <zacharytq@gmail.com>
2019-08-17 00:44:05 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,9 +1,7 @@
 # require libraries/modules here
 require "Nokogiri"
 
-html = File.read('fixtures/kickstarter.html')
-kickstarter = Nokogiri::HTML(html)
-
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
 end
